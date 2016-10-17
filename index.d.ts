3035d951314dea95f0ca6ac66d3944077f24734e
Charles Samborski <demurgos@demurgos.net>
2016-10-17 21:03:14 +0200

    Replace `import *` by  `import =`

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,19 @@
-import * as _vinyl from 'vinyl';
+import vinyl = require("vinyl");
+import chalk = require("chalk");
+import dateformat = require("dateformat");
+// import beeper = require("beeper");
+// import replaceExt = require("replace-ext");
 
-
-import * as _chalk from 'chalk';
-
-import * as _dateformat from 'dateformat';
-
-// import * as _beeper from 'beeper';
-// declare const _beeper: any;
-
-export const File: typeof _vinyl;
-
-// import * as _replaceExtension from 'replace-ext';
+export const File: typeof vinyl;
+export type File = typeof vinyl;
 /**
  * Replaces a file extension in a path. Returns the new path.
  */
 export function replaceExtension(path: string, newExtension: string): string;
-
-export const colors: typeof _chalk;
-export const date: typeof _dateformat;
-export type date = typeof _dateformat;
+export const colors: typeof chalk;
+export type colors = typeof chalk;
+export const date: typeof dateformat;
+export type date = typeof dateformat;
 export {log} from './lib/log';
 export {template} from './lib/template';
 export {env} from './lib/env';
--- lib/noop.d.ts
+++ lib/noop.d.ts
@@ -1,6 +1,7 @@
-import * as through2 from 'through2';
+import through2 = require('through2');
 
 /**
  * Returns a stream that does nothing but pass data straight through.
  */
 export const noop: typeof through2;
+export type noop = typeof through2;
